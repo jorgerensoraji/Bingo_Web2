bbcf673271538228986936015dbf043fef56b2c5
jorgerensoraji <44627216+jorgerensoraji@users.noreply.github.com>
2026-04-19 23:46:03 -0400

    test whatsapp send

--- check_winners.py
+++ check_winners.py
@@ -1,13 +1,7 @@
-import sqlite3, json
-db = sqlite3.connect('/var/www/bingopro/bingo.db')
-
-print("=== SESSIONS TABLE COLUMNS ===")
-for r in db.execute("PRAGMA table_info(sessions)").fetchall():
-    print(f"  {r[1]} ({r[2]})")
-
-print("\n=== SESSION 613AFE9E ALL DATA ===")
-cols = [r[1] for r in db.execute("PRAGMA table_info(sessions)").fetchall()]
-for r in db.execute("SELECT * FROM sessions WHERE id='613AFE9E'").fetchall():
-    for col, val in zip(cols, r):
-        if val and col not in ('cartillas_json',):
-            print(f"  {col}: {str(val)[:200]}")
+import sys
+sys.path.insert(0, '/var/www/bingopro')
+from app import enviar_whatsapp, TWILIO_WA_TEMPLATE_SID
+phone = '+15142924169'
+ok, err = enviar_whatsapp(phone, '', content_sid=TWILIO_WA_TEMPLATE_SID, content_variables={'1': 'Jorge', '2': '2026-04-20 21:00', '3': 'https://ganaperu.com/cartillas'})
+print('OK - message sent!' if ok else f'ERROR: {err}')
+print(f'Template SID used: {TWILIO_WA_TEMPLATE_SID}')
