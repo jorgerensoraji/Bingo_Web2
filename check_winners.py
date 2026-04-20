fd32c6f25e1aaf7e9c715e524bc1f807c5633121
jorgerensoraji <44627216+jorgerensoraji@users.noreply.github.com>
2026-04-20 00:13:37 -0400

    Remove debug script check_winners.py

--- check_winners.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.path.insert(0, '/var/www/bingopro')
-from app import enviar_whatsapp, TWILIO_WA_TEMPLATE_SID
-phone = '+15142924169'
-ok, err = enviar_whatsapp(phone, '', content_sid=TWILIO_WA_TEMPLATE_SID, content_variables={'1': 'Jorge', '2': '2026-04-20 21:00', '3': 'https://ganaperu.com/cartillas'})
-print('OK - message sent!' if ok else f'ERROR: {err}')
-print(f'Template SID used: {TWILIO_WA_TEMPLATE_SID}')
